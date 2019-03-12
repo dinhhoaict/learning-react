54d69ac49b8fde390425e331aa20929d44c586e5
hoaibd1 <hoaibd1@vttek.vn>
2019-03-12 10:10:19 +0700

    tach app

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export class App extends React.Component {
+    render () {
+        let i = 100;
+        const a = 100;
+        return <h1>Hello World from React boilerplate aaaaa {i} {a}</h1>;
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom';
-class Welcome extends React.Component {
-    render () {
-        let i = 100;
-        const a = 100;
-        return <h1>Hello World from React boilerplate aaaaa {i} {a}</h1>;
-    }
-}
-ReactDOM.render(<Welcome />, document.getElementById('root'));
+import {App} from './App';
+ReactDOM.render(<App />, document.getElementById('root'));
